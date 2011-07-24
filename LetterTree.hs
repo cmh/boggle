448a416eb9a8c1113c218e4109e84b8281dae8f1
Chris Holdsworth <chrisholdsworth@gmail.com>
2011-07-24 19:38:10 +0100

    Working LetterTree implementation with insertion/lookup

--- LetterTree.hs
+++ LetterTree.hs
@@ -4,12 +4,12 @@ module LetterTree
 --A restricted trie, where ever branch node can only take one char
 --larger space requirements, but easier conceptually
 
-import Prelude hiding (LT)
+import Prelude hiding (LT, lookup)
 import qualified Data.Map as Map
 
 --Differentiate between intermediate node which themselves represent words
 --and those which do not
-data Node = Word | NonWord 
+data Node = Word | NonWord
 	deriving (Eq, Show)
 
 data LetterTree = LT
@@ -20,15 +20,34 @@ data LetterTree = LT
 topNode = LT NonWord (Map.empty)
 
 insert :: LetterTree -> String -> LetterTree
-insert (LT nt children) [c] = (LT nt (Map.insert c (LT Word Map.empty) children))
+insert lt "" = lt
+insert (LT nt children) [c] = case Map.lookup c children of
+	Just lt -> (LT nt (Map.adjust (\(LT nt children') -> (LT Word children')) c children))
+	Nothing -> (LT nt (Map.insert c (LT Word Map.empty) children))
 insert (LT nt children) (c:cs) = case Map.lookup c children of
 	Just lt -> (LT nt (Map.adjust (const (insert lt cs)) c children))
 	Nothing -> (LT nt (Map.insert c (insert topNode cs) children))
 
 instance Show LetterTree where
-	show (LT nt children) = show nt ++ show children
+	show lt = showAtLevel 0 lt where
+		showAtLevel n (LT nt children) = show nt ++ "\n" ++ concatMap ((concat $ replicate n "--") ++) [k : " " ++ showAtLevel (n+1) a | (k, a) <- Map.assocs children]
 
---data Query = Word | Prefix | None
+data Query = FullWord | Prefix | None
+	deriving (Eq, Show)
+
+lookup :: LetterTree -> String -> Query
+lookup (LT nt children) [] = case nt of
+	Word -> FullWord
+	NonWord -> case Map.null children of
+		True -> error "Whaaaattt?!" --Get rid of this
+		False -> Prefix
+lookup (LT nt children) (c:cs) = case Map.lookup c children of
+	Nothing -> None
+	Just a -> lookup a cs
+
+fromList :: [String] -> LetterTree
+fromList [] = topNode
+fromList (s:ss) = insert (fromList ss) s
 
 testTree = insert (insert (insert topNode "ape") "apple") "bee"
 testTree2 = insert topNode "abcd"
