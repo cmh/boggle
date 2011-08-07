2f0f8c7ced2f7874b88d3ad74859635f75c2de4c
Chris Holdsworth <chrisholdsworth@gmail.com>
2011-08-07 16:54:02 +0100

    cabal tidy and rename LetterTree to Trie

--- Trie.hs
+++ Trie.hs
@@ -1,19 +1,80 @@
 module Trie
     where
 
-import qualified Data.Map as Map
+--A restricted trie, where ever branch node can only take one char
+--larger space requirements, but easier conceptually, and may have
+--better e.g. cache performance and low level optimisations
 
-data Node = Word | NonWord --E.g. complete words or just intermediate stage
+--TODO - make this an instance of relevant type classes
+--Functor, Folable, TRaversable etc
+--Provide a generic trie, and an efficent unpacked bytestring spcialisation
+
+import Prelude hiding (LT, lookup)
+import qualified Data.Map as Map
+import Control.Monad (liftM2)
+import Data.Binary
+
+--Differentiate between intermediate node which themselves represent words
+--and those which do not
+data Node = Word | NonWord
     deriving (Eq, Show)
 
-data Trie = 
-    { letters   :: [Char]
-    , isword    :: Node
-    , children  :: Map.Map Trie } deriving Eq
-    
-instance Show Trie where
-    show t = unlines $ make_list t where
-        make_list (Trie l w c) = l ++ " (" ++ show w ++ ")\n" : indented_children where
-            indented_children = map ("\t" :) (make_list c)
+data Trie = LT
+    { nodeType :: Node
+    , children :: Map.Map Char Trie}
+    deriving Eq
 
-test = Trie ("a" NonWord Map.fromList [Trie ("t"
+instance Binary Node where
+    put Word    = put (0 :: Word8)
+    put NonWord = put (1 :: Word8)
+
+    get = do 
+        t <- get :: Get Word8
+        case t of
+            0 -> return Word
+            1 -> return NonWord
+
+instance Binary Trie where
+    put (LT nt children) = do
+        put nt
+        put children
+
+    get = liftM2 LT get get
+
+topNode :: Trie --The start of any new tree
+topNode = LT NonWord Map.empty
+
+insert :: Trie -> String -> Trie
+insert lt "" = lt
+insert (LT nt children) [c] = case Map.lookup c children of
+    Just lt -> LT nt (Map.adjust (\(LT nt children') -> (LT Word children')) c children)
+    Nothing -> LT nt (Map.insert c (LT Word Map.empty) children)
+insert (LT nt children) (c:cs) = case Map.lookup c children of
+    Just lt -> LT nt (Map.adjust (const (insert lt cs)) c children)
+    Nothing -> LT nt (Map.insert c (insert topNode cs) children)
+
+instance Show Trie where
+    show lt = showAtLevel 0 lt where
+        showAtLevel n (LT nt children) = show nt ++ "\n" ++ concatMap ((concat $ replicate n "--") ++) [k : ' ' : showAtLevel (n+1) a | (k, a) <- Map.assocs children]
+
+
+data Query = FullWord | Prefix | None
+    deriving (Eq, Show)
+
+lookup :: Trie -> String -> Query
+lookup (LT nt children) [] = case nt of
+    Word -> FullWord
+    NonWord -> Prefix 
+    --case Map.null children of
+    --   True -> error "Impossible"
+    --   False -> Prefix
+lookup (LT nt children) (c:cs) = case Map.lookup c children of
+    Nothing -> None
+    Just a -> lookup a cs
+
+fromList :: [String] -> Trie
+fromList [] = topNode
+fromList (s:ss) = insert (fromList ss) s
+
+--Basic testData
+_testTree = insert (insert (insert topNode "ape") "apple") "bee"
