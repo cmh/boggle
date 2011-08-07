321046403689e7166fba6d59ef1552ed94c2a26b
Chris Holdsworth <chrisholdsworth@gmail.com>
2011-08-07 16:48:17 +0100

    Tidyup

--- LetterTree.hs
+++ LetterTree.hs
@@ -2,7 +2,12 @@ module LetterTree
     where
 
 --A restricted trie, where ever branch node can only take one char
---larger space requirements, but easier conceptually
+--larger space requirements, but easier conceptually, and may have
+--better e.g. cache performance and low level optimisations
+
+--TODO - make this an instance of relevant type classes
+--Functor, Folable, TRaversable etc
+--Provide a generic trie, and an efficent unpacked bytestring spcialisation
 
 import Prelude hiding (LT, lookup)
 import qualified Data.Map as Map
@@ -14,6 +19,11 @@ import Data.Binary
 data Node = Word | NonWord
     deriving (Eq, Show)
 
+data LetterTree = LT
+    { nodeType :: Node
+    , children :: Map.Map Char LetterTree}
+    deriving Eq
+
 instance Binary Node where
     put Word    = put (0 :: Word8)
     put NonWord = put (1 :: Word8)
@@ -24,26 +34,6 @@ instance Binary Node where
             0 -> return Word
             1 -> return NonWord
 
-data LetterTree = LT
-    { nodeType :: Node
-    , children :: Map.Map Char LetterTree}
-    deriving Eq
-
-topNode = LT NonWord (Map.empty)
-
-insert :: LetterTree -> String -> LetterTree
-insert lt "" = lt
-insert (LT nt children) [c] = case Map.lookup c children of
-    Just lt -> (LT nt (Map.adjust (\(LT nt children') -> (LT Word children')) c children))
-    Nothing -> (LT nt (Map.insert c (LT Word Map.empty) children))
-insert (LT nt children) (c:cs) = case Map.lookup c children of
-    Just lt -> (LT nt (Map.adjust (const (insert lt cs)) c children))
-    Nothing -> (LT nt (Map.insert c (insert topNode cs) children))
-
-instance Show LetterTree where
-    show lt = showAtLevel 0 lt where
-        showAtLevel n (LT nt children) = show nt ++ "\n" ++ concatMap ((concat $ replicate n "--") ++) [k : " " ++ showAtLevel (n+1) a | (k, a) <- Map.assocs children]
-
 instance Binary LetterTree where
     put (LT nt children) = do
         put nt
@@ -51,15 +41,33 @@ instance Binary LetterTree where
 
     get = liftM2 LT get get
 
+topNode :: LetterTree --The start of any new tree
+topNode = LT NonWord Map.empty
+
+insert :: LetterTree -> String -> LetterTree
+insert lt "" = lt
+insert (LT nt children) [c] = case Map.lookup c children of
+    Just lt -> LT nt (Map.adjust (\(LT nt children') -> (LT Word children')) c children)
+    Nothing -> LT nt (Map.insert c (LT Word Map.empty) children)
+insert (LT nt children) (c:cs) = case Map.lookup c children of
+    Just lt -> LT nt (Map.adjust (const (insert lt cs)) c children)
+    Nothing -> LT nt (Map.insert c (insert topNode cs) children)
+
+instance Show LetterTree where
+    show lt = showAtLevel 0 lt where
+        showAtLevel n (LT nt children) = show nt ++ "\n" ++ concatMap ((concat $ replicate n "--") ++) [k : ' ' : showAtLevel (n+1) a | (k, a) <- Map.assocs children]
+
+
 data Query = FullWord | Prefix | None
     deriving (Eq, Show)
 
 lookup :: LetterTree -> String -> Query
 lookup (LT nt children) [] = case nt of
     Word -> FullWord
-    NonWord -> case Map.null children of
-        True -> error "Whaaaattt?!" --Get rid of this
-        False -> Prefix
+    NonWord -> Prefix 
+    --case Map.null children of
+    --   True -> error "Impossible"
+    --   False -> Prefix
 lookup (LT nt children) (c:cs) = case Map.lookup c children of
     Nothing -> None
     Just a -> lookup a cs
@@ -68,4 +76,5 @@ fromList :: [String] -> LetterTree
 fromList [] = topNode
 fromList (s:ss) = insert (fromList ss) s
 
-testTree = insert (insert (insert topNode "ape") "apple") "bee"
+--Basic testData
+_testTree = insert (insert (insert topNode "ape") "apple") "bee"
--- boggle.hs
+++ boggle.hs
@@ -1,53 +1,48 @@
 {-# LANGUAGE BangPatterns #-}
-module Main
+module Board
     where
 
 import Data.List (sortBy, intersect, intersperse, nub, (\\), sort)
 import qualified Data.IntMap as Map
 import Control.Monad (liftM)
-import System.Directory (doesFileExist)
-import Data.Binary (encodeFile, decodeFile)
-import LetterTree
 import System.Random
-import System.CPUTime
 import Data.Maybe (fromJust)
-import Data.Sequence ()
 
-data Board = Board
-    { size :: Int
-    , board :: [Char]
-    } deriving (Eq)
-
-{-data Cell = Cell-}
-    {-{ id :: Int-}
-    {-, char :: Char-}
-    {-, neighbours :: [Cell] }-}
+import LetterTree
 
 data Cell = Cell
     { char :: Char
     , neighbours :: [Int] } 
     deriving (Show, Eq)
 
---This is probably slow due to non integer keys
+data Board = Board
+    { size :: Int
+    , board :: String
+    } deriving (Eq)
+
+instance Show Board where
+    show (Board n ps) = sn ++ " * " ++ sn ++ " board:" ++ hLine ++ sb ps ++ hLine where
+        sn = show n
+        hLine = '\n' : replicate (2*n+1) '-'
+        sb [] = ""
+        sb ps = "\n|" ++ intersperse '|' (take n ps) ++ '|' : sb (drop n ps)
+
+--BoardNeighbours is an additional datastructure which
+--caches from frequently needed relational information about the board
+--i.e. all valid neighbours of a given cell
+--This is could be improved by using integer keys
 type BoardNeighbours = Map.IntMap Cell
 
 fromBoard :: Board -> BoardNeighbours
 fromBoard (Board n ps) = Map.fromList $ map (makeCell ps) [0 .. (n*n-1)] where
     makeCell ps k = (k, Cell c ns) where
-        c = {-# SCC "GetLetter" #-} ps !! k --Doesn't scale well to large boards
+        c = {-# SCC "GetLetter" #-} ps !! k --Doesn't scale well to large boards (c.f. Data.Vector)
         (j, i) = {-# SCC "DivMod" #-} k `divMod` n
-        ns = {-# SCC "SortNeighbours" #-} sort ns'
+        ns = {-# SCC "SortNeighbours" #-} sort ns' --Sorted to allow efficiient diffs (see diff, ins below) (could encode in a type class)
         ns' ={-# SCC "MakeNeighbours" #-} makeNeighbours i j
         makeNeighbours i j = sort $ map (\(i, j) -> j*n + i) $ 
-                             filter (\(x, y) -> (x >= 0 && y >= 0 && x < n && y < n)) $
-                             [(i+1, j), (i-1, j), (i+1, j-1), (i-1, j-1), (i, j-1), (i-1,j+1), (i, j+1), (i+1, j+1)] --Could do this sorting by hand
-
-instance Show Board where
-    show (Board n ps) = sn ++ " * " ++ sn ++ " board:" ++ hLine ++ (sb ps) ++ hLine where
-        sn = show n
-        hLine = '\n' : replicate (2*n+1) '-'
-        sb [] = ""
-        sb ps = "\n|" ++ (intersperse '|' $ take n ps) ++ '|' : sb (drop n ps)
+                             filter (\(x, y) -> (x >= 0 && y >= 0 && x < n && y < n))
+                             [(i+1, j), (i-1, j), (i+1, j-1), (i-1, j-1), (i, j-1), (i-1,j+1), (i, j+1), (i+1, j+1)] --Could do this sorting by hand for a microoptimisation
 
 --Make better names for these things, could easily make this concurrent
 allWords b letterTree = allWords' b boardAnnotations where 
@@ -64,6 +59,7 @@ allWords b letterTree = allWords' b boardAnnotations where
                 visited' = {-# SCC "NewVisited" #-} ins loc visited  --Could just use a vector
                 moreWords = concatMap (go word visited') ns'
 
+--Functions to facilitate efficient list difference over (//)
 diff [] _ = []
 diff xs [] = xs
 diff !(x:xs) (y:ys) | x < y  = x : diff xs (y:ys)
@@ -73,76 +69,26 @@ diff !(x:xs) (y:ys) | x < y  = x : diff xs (y:ys)
 ins i [] = [i]
 ins i !(x:xs) | i < x = i : x : xs
               | i > x = x : ins i xs
-              | otherwise = (x : xs) --Should never be hit
+              | otherwise = x : xs --Should never be hit, could error in debug
 
-b4 = Board 4 "abcdefghijklmnop"
-b5 = Board 5 "fdkrpvmerlksjdmepowrnckda"
 
+--Generation of random boards with each letter uniformly distributed over [a,z]
+--Should change at some point to reflect real boggle construction
 randomBoard :: Int -> IO Board
 randomBoard n = do
     gen <- newStdGen
-    let chars = randomRs ('a', 'z') gen :: [Char]
+    let chars = randomRs ('a', 'z') gen :: String
     let boardString = take (n*n) chars
     return (Board n boardString)
 
 generateBoards :: Int -> Int -> IO [Board]
 generateBoards size n = mapM randomBoard (replicate n size)
 
-best_words :: Board -> LetterTree -> [String]
-best_words b lt = reverse . (sortBy compLength) . nub . (filter ((> 3) . length)) $ allWords b lt where
-    compLength a b = (length a) `compare` (length b)
+--Utility functions to extract num of solution and best solutions
+bestWords :: Board -> LetterTree -> [String]
+bestWords b lt = reverse . sortBy compLength . nub . filter ((> 3) . length) $ allWords b lt where
+    compLength = Data.Ord.comparing length
 
-num_words b lt = length . nub . (filter ((> 3) . length)) $ allWords b lt 
+numWords :: Board -> LetterTree -> Int
+numWords b lt = length . nub . filter ((> 3) . length) $ allWords b lt 
 
-
-treeLocation, dictLocation :: FilePath
-treeLocation = "/home/chris/.boggle/wordTree.bin"
-dictLocation = "/home/chris/Desktop/WORD.LST"
-
-createWordTreeFile :: IO ()
-createWordTreeFile = do
-    wordList <- readFile dictLocation
-    let wordTree = (fromList . lines) wordList
-    encodeFile treeLocation wordTree
-
-
-wordTree :: IO LetterTree
-wordTree = do
-    wordTreeExists <- doesFileExist treeLocation
-    case wordTreeExists of
-        True -> decodeFile treeLocation
-        False -> do 
-            putStrLn $ "Creating wordTree in " ++ treeLocation
-            createWordTreeFile 
-            decodeFile treeLocation
-
-solveBoards = do
-    wt <- wordTree
-    putStrLn $ "Generating boards"
-    let n = 50000
-    boards <- generateBoards 4 n
-    let len = length boards
-    putStrLn $ "Generated " ++ show len ++ " random boards"
-    start <- getCPUTime
-    let !s = (sum . (map (flip num_words wt))) boards
-    finish <- getCPUTime
-    putStrLn $ show s ++ " total words"
-    let runTime = (finish - start) `div` 10^9
-    putStrLn $ "Solved " ++ show n ++ " boards in " ++ show runTime ++ "ms"
-    let rate = fromIntegral (n * 1000) / (fromIntegral runTime)
-    putStrLn $ "Rate of " ++ show rate ++ "b/s"
-
-print_solutions board = do
-    print board
-    wt <- wordTree
-    let numSols = num_words board wt
-    let bw = best_words board wt
-    putStrLn $ show numSols ++ " solutions\n"
-    putStrLn $ "Best 100 solutions\n"
-    putStrLn . unlines . (take 100) $ bw
-
-main = do
-    board <- randomBoard 100
-    print_solutions board
-
-{-main = solveBoards-}
